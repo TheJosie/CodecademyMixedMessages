cc32e6ede985ea95d0de98ba1096963f6163147a
JosieS <sonesjosie@gmail.com>
2025-04-13 20:18:17 -0500

    First Commit, added the 8-ball

---
+++ Messages.js
@@ -0,0 +1,21 @@
+
+//generates random response
+genResponse = Math.floor(Math.random() * 8);us
+//response that generates
+if (genResponse === 0) {
+    console.log("Why not? Logic clearly isn’t part of this.");
+} else if (genResponse === 1) {
+    console.log("I mean… you do you. Just don’t blame me later.");
+} else if (genResponse === 2) {
+    console.log("Absolutely. And unicorns are real too.");
+} else if (genResponse === 3) {
+    console.log("Better not tell you now… because you’ll probably cry.");
+} else if (genResponse === 4) {
+    console.log("You’re adorable when you’re delusional.");
+} else if (genResponse === 5) {
+    console.log("Yeah, keep dreaming, superstar.");
+} else if (genResponse === 6) {
+    console.log("Ask again when you’re ready to hear the truth.");
+} else if (genResponse === 7) {
+    console.log("Signs point to yes — not that you’d understand them.");
+}
